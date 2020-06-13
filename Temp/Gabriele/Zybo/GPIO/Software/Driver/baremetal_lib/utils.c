207d025404c3d26b96114526c2a7159dcef7b3c9
jiin995 <gabrieleprevitera@gmail.com>
2020-06-13 11:24:45 +0200

    Add funzioni di utilità per registrare interrupt

--- Temp/Gabriele/Zybo/GPIO/Software/Driver/baremetal_lib/utils.c
+++ Temp/Gabriele/Zybo/GPIO/Software/Driver/baremetal_lib/utils.c
@@ -1,9 +1,10 @@
 #include "utils.h"
+#include "xscugic.h"
+
 /*TODO
 * METTERE ASSERT
 *
 */
-
 
 //Scrive lo stesso bit nelle posizioni desiderate
 uint32_t write_bit_in_pos(uint32_t* address, uint32_t pos, uint32_t bit){
@@ -24,4 +25,44 @@ uint8_t  read_bit_in_single_pos(uint32_t* address, uint8_t pos){
         return (*(address) & pos) && 1;
     }
     return 0;
+}
+
+XScuGic gic_enable(uint32_t GIC_ID){
+    XScuGic_Config * gic_conf ;
+    XScuGic gic_inst;
+
+    //Configuriamo il GIC
+    gic_conf = XScuGic_LookupConfig(GIC_ID);
+	int status = XScuGic_CfgInitialize(&gic_inst, gic_conf, gic_conf->CpuBaseAddress);
+
+	if (status != XST_SUCCESS){
+		return 0;
+	}
+
+	//abilitazione del gic per gestire gli interrupt esterni
+	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT,(Xil_ExceptionHandler)XScuGic_InterruptHandler,( void*) &gic_inst);
+	Xil_ExceptionEnable();
+
+    return gic_inst;
+}
+
+int32_t gic_disable(uint32_t GIC_ID){
+    return Xil_ExceptionDisable();
+}
+
+uint32_t *gic_register_interrupt(XScuGic gic_inst ,uint32_t interrupt_line, void* interrupt_handler){
+
+    int status = XScuGic_Connect(&gic_inst, interrupt_line, (Xil_InterruptHandler)interrupt_handler, ( void*) &gic_inst);
+	if (status != XST_SUCCESS){
+			return status;
+		}
+    
+    XScuGic_Enable( &gic_inst, interrupt_line);
+
+    return XST_SUCCESS 
+}
+
+uint32_t *gic_register_interrupt_handler(XScuGic gic_inst, interrupt_handler* interrupt_handler){
+    return gic_register_interrupt(XScuGic gic_inst , interrupt_handler->interrupt_line, interrupt_handler->interrupt_handler){
+
 }
--- Temp/Gabriele/Zybo/GPIO/Software/Driver/baremetal_lib/utils.h
+++ Temp/Gabriele/Zybo/GPIO/Software/Driver/baremetal_lib/utils.h
@@ -2,8 +2,24 @@
 #define UTILS_H
 
 #include "stdlib.h"
+#include "xscugic.h"
+
+
+typedef struct{
+    uint32_t interrupt_line;
+    void* interrupt_handler(void* data);
+}interrupt_handler;
 
 //Scrive lo stesso bit nelle posizioni desiderate
 uint32_t write_bit_in_pos(uint32_t* address, uint32_t pos, uint32_t bit);
 uint8_t  read_bit_in_single_pos(uint32_t* address, uint8_t pos);
+
+
+XScuGic gic_enable(uint32_t GIC_ID);
+int32_t gic_disable(uint32_t GIC_ID);
+
+uint32_t *gic_register_interrupt(XScuGic gic_inst, uint32_t interrupt_line, void* interrupt_handler);
+uint32_t *gic_register_interrupt_handler(XScuGic gic_inst, interrupt_handler* interrupt_handler);
+
+
 #endif
