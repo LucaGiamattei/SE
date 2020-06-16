89ce897bad65e6efa7109fab44de7aadb089cf6a
jiin995 <gabrieleprevitera@gmail.com>
2020-06-16 11:28:46 +0200

    Update with usage

---
+++ Temp/Gabriele/Zybo/GPIO/Software/Driver/no_driver/init_build_environment.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+########################################################################################
+# Configura il build envirnoment per cross compilare usando il compilatore di buildroot
+########################################################################################
+#
+# USO
+# Cambiare BUILDROOT_PATH con il path di buildroot
+# dare source init_build_environment.sh
+
+BUILDROOT_PATH="/home/giorgio/Documents/SO_embedded/Zinco-Linux/buildroot-2019.02.5/output/host/bin"
+
+export CROSS_COMPILE=arm-buildroot-linux-uclibcgnueabihf-
+export PATH=$PATH:${BUILDROOT_PATH}
+export CXX=${CROSS_COMPILE}g++
+export CC=${CROSS_COMPILE}gcc
+export LD=${CROSS_COMPILE}ld
+export AR=${CROSS_COMPILE}ar
+export OBJCOPY=${CROSS_COMPILE}objcopy
+export SIZE=${CROSS_COMPILE}size
